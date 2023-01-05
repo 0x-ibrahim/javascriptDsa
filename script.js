324fd1cbd9edacb8acaada6050751030f186a8c2
onuchecodes <onucheumar@yahoo.com>
2023-01-05 20:19:22 +0100

    Added a Script file

---
+++ script.js
@@ -0,0 +1,12 @@
+//Arrays
+
+var ourArray = ["John", 23]
+var  myArray = ["Ibrahim",21];
+
+/*
+Nested Array or Multiline arrays these refres to when you an array inside of an array
+*/
+
+let nestedArray = [["The Universe", 23], ["The Multiverse", 10101]]
+
+var mySetup
